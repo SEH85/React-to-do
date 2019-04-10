e2f18c7eb266f9bbeb52384022d193080d5e8b61
SEH85 <sean.hellyer@gmail.com>
2019-04-09 21:10:19 -0400

    adding events assignment

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,6 +4,7 @@ class ToDo extends Component {
 render() {
     return (
         <li>
+        <button type="button" onClick={ this.props.deleteToDo} > delete </button>
         <input type="checkbox" checked={ this.props.isCompleted } />
         <span>{ this.props.description }</span>
       </li>   
